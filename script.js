e115e7598b18d0095e0e5e0d2fcfddc0f835624b
Anja Wurlitzer <anja.wurlitzer.coding@gmail.com>
2022-12-14 18:55:19 +0100

    fullfil the tasks

---
+++ script.js
@@ -0,0 +1,29 @@
+// console.log("Hello World");
+
+let myName = "Anja Wurlitzer";
+console.log(myName);
+
+let greeting = "Hallo ";
+console.log(greeting + myName);
+
+const netPrice = 25;
+const tax = 19;
+let grossPrice;
+grossPrice = (netPrice * tax) / 100;
+console.log(grossPrice);
+grossPrice = grossPrice + netPrice;
+console.log(grossPrice);
+
+// console.log("Hallo"; <- Klammer zu!
+
+// const a = 1;
+// a = 2; <- Konstante hat bereits Wert 1 und kann keinen anderen annehmen.
+
+// const 1 = "a"; <- Variable darf nicht mit Zahl oder Unterstrich beginnen.
+
+// const let = undefined; <- let ist reserved Keyword und kann kein Variablenname sein.
+
+// return;
+// console.log("..."); -> return bezieht sich auf Funktionen.
+
+// console.log("That doesn"t work"); -> keine quotation marks in der Mitte des string Werts.
