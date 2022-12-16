f751e25fe59537ee84520c9da010599c5cd73a07
Anja Wurlitzer <anja.wurlitzer.coding@gmail.com>
2022-12-16 19:55:28 +0100

    tasks day 3

--- script.js
+++ script.js
@@ -95,6 +95,7 @@ console.log(fooBar("bar"));
 console.log(fooBar("xxx"));
 console.log(fooBar("aa"));
 
+//Function count the sheeps
 const sheeps = [true, false, true, false, true, true, false, 3, 18, true, true];
 let countSheeps = 0;
 
@@ -104,3 +105,95 @@ for (let i = 0; i < sheeps.length; i = i + 1) {
   }
 }
 console.log(countSheeps);
+
+//Tasks day 3 https://github.com/coding-bootcamps-eu/introduction-to-programming-in-js/tree/main/tasks/day3
+
+//counts spaces in string
+function countSpaces(text) {
+  if (typeof text === "string") {
+    let x = 0;
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] === " ") {
+        x++;
+      }
+    }
+    return x;
+  }
+}
+
+console.log(countSpaces("db jz "));
+
+//string contains Umlauts?
+function hasUmlauts(word) {
+  if (typeof word === "string") {
+    let x = 0;
+    for (let i = 0; i < word.length; i++) {
+      if (
+        word[i] === "ä" ||
+        word[i] === "ö" ||
+        word[i] === "ü" ||
+        word[i] === "Ü" ||
+        word[i] === "Ä" ||
+        word[i] === "Ö" ||
+        word[i] === "ß"
+      ) {
+        x++;
+      }
+    }
+    if (x > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+console.log(hasUmlauts("DDGFH"));
+
+// or:
+function hasUmlauts(word) {
+  if (typeof word === "string") {
+    const ä = "ä";
+    const Ä = "Ä";
+    const ü = "ü";
+    const Ü = "Ü";
+    const ö = "ö";
+    const Ö = "Ö";
+    const ß = "ß";
+
+    if (
+      word.includes(ä) ||
+      word.includes(Ä) ||
+      word.includes(ü) ||
+      word.includes(Ü) ||
+      word.includes(ö) ||
+      word.includes(Ö) ||
+      word.includes(ß)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+console.log(hasUmlauts("sö"));
+
+//is string in uppercase?
+function isUpperCase(word) {
+  if (typeof word === "string") {
+    const WORD = word.toLocaleUpperCase(); /*turns word into Uppercase*/
+    if (word === WORD) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+console.log(isUpperCase("HRJK"));
+
+//how long is the string?
+function stringLength(word) {
+  if (typeof word === "string") {
+    return word.length;
+  }
+}
+console.log(stringLength("asrdtfhg"));
